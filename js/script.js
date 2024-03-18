6a58e078958784d4230df88355257140fd86d810
= <giacomo.bartoli@me.com>
2024-03-18 18:34:51 +0100

    final beta

--- js/script.js
+++ js/script.js
@@ -7,8 +7,6 @@ window.onload = () => {
         location.reload()
     })
 
-    items.length > 0 ?  cart(items) : '';
-
     searchFormControls();
 
     getBooks()
@@ -30,10 +28,12 @@ const getBooks = async () => {
         return data
 
     } catch (error) {
-        console.log(error);
+        console.log(error)
+    
     } finally {
         hideSpinner();
         showResults();
+        items.length > 0 ?  cart(items) : emptyCart();
     }
 }
 
@@ -46,18 +46,24 @@ const showResults = () => {
 }
 
 const createMainCards = (book, section) => {
-    let {img, title, price } = book;
+    let {img, title, price} = book;
 
-    console.log(section);
     const popularResults = document.querySelector(section);
-    console.log(popularResults);
 
     const col = document.createElement('div');
     section !== '.search-results' ? col.classList.add('col', 'swiper-slide') : col.classList.add('col') ;
-    console.log(col);
     
     const card = document.createElement('div');
     card.classList.add('card', 'pt-3', 'px-3', 'border-0', 'rounded-4', 'main-card');
+
+    const badgeCard = document.createElement('span');
+    badgeCard.classList.add('badge-card');
+
+    items.forEach(item => {
+        if (item.title === title ) {
+            badgeCard.classList.add('active');
+        } 
+    })
 
     const imgCard = document.createElement('img');
     imgCard.classList.add('object-fit-cover', 'w-100', 'h-100', 'card-img');
@@ -73,14 +79,25 @@ const createMainCards = (book, section) => {
     const cardText = document.createElement('div');
     cardText.classList.add('card-text', 'd-flex', 'justify-content-center');
 
-    cardText.innerHTML = /* HTML */`
-        <a href="#" class="bg--red text-white px-2 rounded-pill  searchbar__button text-center text-decoration-none " role="button" onclick="addItemToCart('${title}', ${price})">
-            <ion-icon name="cart-outline"></ion-icon><span class="small ps-2 p-0">${price} $</span>
-        </a>
-    `
+    const buttonCard = document.createElement('button');
+    buttonCard.classList.add('btn','bg--red','text-white', 'px-2', 'rounded-pill', 'searchbar__button');
 
+    buttonCard.addEventListener('click', () => {
+        addItemToCart(title, price);
+        badgeCard.classList.add('active');
+    })
+    
+    const spanPrice = document.createElement('span');
+    spanPrice.classList.add('small', 'ps-2', 'p-0')
+    spanPrice.innerHTML = price + ' $';
+    
+    const cartIcon = document.createElement('span');
+    cartIcon.innerHTML = `<ion-icon name="cart-outline"></ion-icon>`;
+
+    buttonCard.append(cartIcon, spanPrice)
+    cardText.append(buttonCard);
     cardBody.append(titleCard, cardText);
-    card.append(imgCard, cardBody);
+    card.append(badgeCard, imgCard, cardBody);
     col.append(card);
     popularResults.append(col);
 }
@@ -90,36 +107,75 @@ const createMiniatureCards = (book, section) => {
 
     const results = document.querySelector(section);
 
+    const col = document.createElement('div');
+    col.classList.add('col', 'py-3', 'swiper-slide');
+    
     const card = document.createElement('div');
-    card.classList.add('col', 'py-3', 'swiper-slide');
+    card.classList.add('card', 'flex-row', 'card--white', 'border-0', 'p-0');
+    
+    const badgeCard = document.createElement('span');
+    badgeCard.classList.add('badge-card', 'badge-card--miniature');
 
-    card.innerHTML = /* HTML */ `
-        <div class="card flex-row card--white border-0  p-0 ">
-            <div class="row align-items-center ">
-                <div class="col-4">
-                    <div class="miniature__container ">
-                        <div class="miniature">
-                            <img src="${img}" alt="" class="object-fit-cover w-100 h-100 card-img">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-8">
-                    <div class="card-body p-0 ps-2">
-                        <div class="card-title">
-                            <h5 class="small  ">${title}</h5>
-                        </div>
-                        <div class="card-text d-flex justify-content-start">
-                            <a href="#" class="bg--red text-white px-3 rounded-pill  searchbar__button text-center text-decoration-none " role="button" onclick="addItemToCart('${title}', ${price})">
-                                <ion-icon name="cart-outline"></ion-icon><span class="small ps-2 p-0">${price}&nbsp;$</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `;
+    items.forEach(item => {
+        if (item.title === title ) {
+            badgeCard.classList.add('active');
+        } 
+    })
+    
+    const row = document.createElement('div');
+    row.classList.add('row', 'align-items-center');
+    
+    const col4 = document.createElement('div');
+    col4.classList.add('col-4');
+    
+    const miniatureContainer = document.createElement('div');
+    miniatureContainer.classList.add('miniature__container');
+    
+    const miniature = document.createElement('div');
+    miniature.classList.add('miniature');
+    miniature.innerHTML = `<img src="${img}" alt="" class="object-fit-cover w-100 h-100 card-img">`;
 
-    results.append(card)
+    
+    const col8 = document.createElement('div');
+    col8.classList.add('col-8');
+    
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body', 'p-0', 'ps-2');
+    
+    const cardTitle = document.createElement('div');
+    cardTitle.classList.add('card-title');
+    cardTitle.innerHTML = `<h5 class="small  ">${title}</h5>`
+    
+    const cardText = document.createElement('div');
+    cardText.classList.add('card-text', 'd-flex', 'justify-content-start');
+    
+    
+    const buttonCard = document.createElement('button');
+    buttonCard.classList.add('btn','bg--red','text-white', 'px-2', 'rounded-pill', 'searchbar__button');
+    
+    buttonCard.addEventListener('click', () => {
+        addItemToCart(title, price);
+        badgeCard.classList.add('active')  
+    })
+    
+    const spanPrice = document.createElement('span');
+    spanPrice.classList.add('small', 'ps-2', 'p-0')
+    spanPrice.innerHTML = price + ' $';
+    
+    const cartIcon = document.createElement('span');
+    cartIcon.innerHTML = `<ion-icon name="cart-outline"></ion-icon>`;
+    
+    
+    results.append(col)
+    col.append(card)
+    card.append(badgeCard, row);
+    row.append(col4, col8);
+    col4.append(miniatureContainer);
+    miniatureContainer.append(miniature);
+    col8.append(cardBody);
+    cardBody.append(cardTitle, cardText);
+    cardText.append(buttonCard);
+    buttonCard.append(cartIcon, spanPrice)
 }
 
 const addItemToCart = (title, price) => {
@@ -137,10 +193,10 @@ const createCartItem = (item) => {
     const itemsList = document.querySelector('.cart__items-list');
     
     const liElement = document.createElement('li');
-    liElement.classList.add('cart__item', 'list-group-item', 'ps-2', 'small', 'd-flex', 'flex-wrap', 'justify-content-between', 'align-items-center')
+    liElement.classList.add('cart__item', 'list-group-item', 'ps-2', 'small', 'd-flex', 'flex-wrap', 'justify-content-between', 'align-items-center', 'flex-row')
     
     liElement.innerHTML = /* HTML */ `
-    <a href="#" class="title text-secondary text-decoration-none text-truncate">${item.title}</a>
+    <a href="#" class="title text-secondary text-decoration-none text-truncate me-2">${item.title}</a>
     <span class="rounded-pill p-2 bg--orange">${item.price}&nbsp;$</span>
     `;
     itemsList.append(liElement)
@@ -148,13 +204,17 @@ const createCartItem = (item) => {
 
 const cart = (items) => {
     document.querySelector('.cart__fully').classList.remove('d-none');
-    document.querySelector('.cart__empty').classList.add('d-none');
     
     items.forEach(item => createCartItem(item))
 
     totalCart = items.reduce((partial, items) => items.price + partial, 0)
 
+    document.querySelector('#totalItems').innerHTML = `${items.length}`
     document.querySelector('.total-price__item').innerHTML = `${totalCart.toFixed(2)} $`
+}
+
+const emptyCart = () => {
+    document.querySelector('.cart__empty').classList.remove('d-none');
 }
 
 const searchItems = (form) => {
@@ -163,9 +223,12 @@ const searchItems = (form) => {
     if(query.length >= 3) {
         let results = allBooks.filter(book => book.title.toLowerCase().includes(query) );
         document.querySelector('.search-results').innerHTML = '';
-        document.querySelector('.search-section').classList.remove('d-none')
-        results.forEach(result => createMainCards(result, '.search-results'))
-        console.log(results);
+        
+        if (results.length > 0) {
+            document.querySelector('.search-section').classList.remove('d-none')
+            results.forEach(result => createMainCards(result, '.search-results'))
+        } 
+    
     } else {
         document.querySelector('.search-section').classList.add('d-none');
         document.querySelector('.search-results').innerHTML = '';
@@ -181,7 +244,7 @@ const searchItemsRealTime = (form) => {
         document.querySelector('.search-results').innerHTML = '';
         document.querySelector('.search-section').classList.remove('d-none')
         results.forEach(result => createMainCards(result, '.search-results'))
-        console.log(results);
+    
     } else {
         document.querySelector('.search-section').classList.add('d-none');
         document.querySelector('.search-results').innerHTML = '';
